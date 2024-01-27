0beaf35538350be2239daf402a4f6e1e34f24055
Rostislav <b.r.rostislav@gmail.com>
2024-01-28 00:22:07 +0200

    task work commit

--- Lesson13.py
+++ Lesson13.py
@@ -2,3 +2,62 @@
 # Зразковий список класів: Person, Teacher, Student, Subject, Academy і т.д.
 # Продумати архітектуру: реалізувати принципи ООП
 
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+class Teacher(Person):
+    def __init__(self, name, age, gender, subject):
+        super().__init__(name, age, gender)
+        self.subject = subject
+
+    def teach(self):
+        print(f"{self.name} is teaching {self.subject}")
+
+class Student(Person):
+    def __init__(self, name, age, gender, grade):
+        super().__init__(name, age, gender)
+        self.grade = grade
+
+    def study(self):
+        print(f"{self.name} is studying")
+
+class Subject:
+    def __init__(self, name, teacher):
+        self.name = name
+        self.teacher = teacher
+
+class Academy:
+    def __init__(self, name, students=None, teachers=None, subjects=None):
+        self.name = name
+        self.students = students or []
+        self.teachers = teachers or []
+        self.subjects = subjects or []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+    def add_teacher(self, teacher):
+        self.teachers.append(teacher)
+
+    def add_subject(self, subject):
+        self.subjects.append(subject)
+
+teacher1 = Teacher("Сухомлинський Василь", 40, "Male", "Math")
+teacher2 = Teacher("Алчевська Христина", 35, "Female", "Physics")
+
+student1 = Student("Сергій", 20, "Male", "A")
+student2 = Student("Анна", 22, "Female", "B")
+
+math_subject = Subject("Mathematics", teacher1)
+physics_subject = Subject("Physics", teacher2)
+
+academy = Academy("My Academy", [student1, student2], [teacher1, teacher2], [math_subject, physics_subject])
+
+for student in academy.students:
+    student.study()
+
+for teacher in academy.teachers:
+    teacher.teach()
